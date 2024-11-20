f6dabb0773c346ef42614a814e3e9fe4aad91434
PshycoReaper <azdx2023@gmail.com>
2024-11-19 18:50:55 -0700

    quiero saber si funciona ahora

--- JS/script.js
+++ JS/script.js
@@ -9,41 +9,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 
-
-
-
-
-
-//DEZPLAZAMIENTO DE 100 EN 100  
-class SectionScroller {
-  constructor(selector) {
-    this.sections = document.querySelectorAll(selector);
-    this.currentSection = 0;
-
-    // Vinculamos el evento de desplazamiento
-    this.initScrollEvent();
-  }
-
-  // Función para hacer scroll a una sección específica
-  scrollToSection(index) {
-    this.sections[index].scrollIntoView({ behavior: "smooth" });
-  }
-
-  // Función para inicializar el evento de desplazamiento
-  initScrollEvent() {
-    window.addEventListener("wheel", (event) => {
-      if (event.deltaY > 0) { // Desplazamiento hacia abajo
-        this.currentSection = Math.min(this.currentSection + 1, this.sections.length - 1);
-      } else { // Desplazamiento hacia arriba
-        this.currentSection = Math.max(this.currentSection - 1, 0);
-      }
-      this.scrollToSection(this.currentSection);
-    });
-  }
-}
-
-// Instancia de la clase para las secciones con la clase .section
-new SectionScroller(".section");
-
-
+c
 
